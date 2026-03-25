5e3528600ee97c2d33c9235aa278d420a87ed066
Herrera <sherreraalzate2@gmail.com>
2026-03-24 21:39:01 -0500

    Ejercicio7

--- Ejercicio5/Program.cs
+++ Ejercicio5/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingresa un número del 1 al 7: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        switch (numero)
+        {
+            case 1:
+                Console.WriteLine("Resultado: Lunes");
+                break;
+            case 2:
+                Console.WriteLine("Resultado: Martes");
+                break;
+            case 3:
+                Console.WriteLine("Resultado: Miércoles");
+                break;
+            case 4:
+                Console.WriteLine("Resultado: Jueves");
+                break;
+            case 5:
+                Console.WriteLine("Resultado: Viernes");
+                break;
+            default:
+                Console.WriteLine("Resultado: Número fuera del rango laboral.");
+                break;
+        }
+    }
+}
--- Ejercicio6/Program.cs
+++ Ejercicio6/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingresa tu salario anual: ");
+        double salario = Convert.ToDouble(Console.ReadLine());
+
+        if (salario > 12000)
+        {
+            double impuesto = (salario - 12000) * 0.15;
+            Console.WriteLine("Resultado: " + impuesto);
+        }
+        else
+        {
+            Console.WriteLine("Resultado: No debe impuestos.");
+        }
+    }
+}
