9defebb66e3235f47278ccc962b12f90c1db7989
Herrera <sherreraalzate2@gmail.com>
2026-03-24 21:23:05 -0500

    Ejercicio2

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingresa el primer número: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingresa el segundo número: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        if (num1 > num2)
+        {
+            Console.WriteLine("Resultado: " + (num1 * 2));
+        }
+        else
+        {
+            Console.WriteLine("Resultado: " + (num2 * 3));
+        }
+    }
+}
