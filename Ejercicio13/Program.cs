281a8ebf4a161e18148dfa6b133d33d378e23cca
Herrera <sherreraalzate2@gmail.com>
2026-03-24 22:02:22 -0500

    Ejercicio14

--- Ejercicio13/Program.cs
+++ Ejercicio13/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingresa una palabra: ");
+        string palabra = Console.ReadLine().ToLower();
+
+        int contador = 0;
+
+        for (int i = 0; i < palabra.Length; i++)
+        {
+            if (palabra[i] == 'a' || palabra[i] == 'e' || palabra[i] == 'i' || palabra[i] == 'o' || palabra[i] == 'u')
+            {
+                contador++;
+            }
+        }
+
+        Console.WriteLine("Resultado: " + contador);
+    }
+}
---
+++ Ejercicio14/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class FactorialFinder
+{
+    static void Main()
+    {
+        Console.Write("Ingrese un número: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        long factorial = 1;
+
+        for (int i = 1; i <= numero; i++)
+        {
+            factorial *= i;
+        }
+
+        Console.WriteLine($"Resultado: {factorial}");
+    }
+}
