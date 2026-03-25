a36867b18578708745c1036ed04a20a3e21f2a71
Herrera <sherreraalzate2@gmail.com>
2026-03-24 21:16:32 -0500

    Ejercicio1

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingresa un número: ");
+        double numero = Convert.ToDouble(Console.ReadLine());
+
+        if (numero > 0)
+        {
+            Console.WriteLine("Resultado: " + (numero * numero));
+        }
+        else if (numero == 0)
+        {
+            Console.WriteLine("Resultado: 0");
+        }
+        else
+        {
+            Console.WriteLine("Resultado: Número negativo.");
+        }
+    }
+}
