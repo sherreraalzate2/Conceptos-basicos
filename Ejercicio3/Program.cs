0b03d5e03bebda451c5fb66dd9d34d535907f2e6
Herrera <sherreraalzate2@gmail.com>
2026-03-24 21:25:04 -0500

    Ejercicio3

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingresa un número: ");
+        double numero = Convert.ToDouble(Console.ReadLine());
+
+        if (numero > 0)
+        {
+            Console.WriteLine("Resultado: " + Math.Sqrt(numero));
+        }
+        else
+        {
+            Console.WriteLine("Resultado: " + (numero * numero));
+        }
+    }
+}
