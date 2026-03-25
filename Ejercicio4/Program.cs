a0a3acfabda2152bf86d6a89049361a892b9e05a
Herrera <sherreraalzate2@gmail.com>
2026-03-24 21:27:12 -0500

    Ejercicio4

---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingresa el radio del círculo: ");
+        double radio = Convert.ToDouble(Console.ReadLine());
+
+        double perimetro = 2 * Math.PI * radio;
+
+        Console.WriteLine("Resultado: " + perimetro.ToString("F2"));
+    }
+}
